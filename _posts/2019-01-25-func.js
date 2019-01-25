8af89cb3c55ea63d518f1741e4b472c1b0a5b81b
bmwhopper <brianmathews.mathews8@gmail.com>
2019-01-25 16:57:56 +0000

    Update 2019-01-25-func.js

--- _posts/2019-01-25-func.js
+++ _posts/2019-01-25-func.js
@@ -10,27 +10,28 @@ set_order: 1
 
 ~~~javascript
 
-const fdk = require(‘@fnproject/fdk’);
-const fs = require(‘fs’);
-const tmp = require(‘tmp’);
-const im = require(‘imagemagick’);
+const fdk = require('@fnproject/fdk');
+const fs  = require('fs');
+const tmp = require('tmp');
+const im  = require('imagemagick');
+
 fdk.handle((buffer, ctx) => {
-return new Promise((resolve, reject) => {
-tmp.tmpName((err, tmpFile) => {
-if (err) throw err;
-fs.writeFile(tmpFile, buffer, (err) => {
-if (err) throw err;
-im.identify([‘-format’, ‘{“width”: %w, “height”: %h}’, tmpFile],
-(err, output) => {
-if (err) {
-reject(err);
-} else {
-resolve(JSON.parse(output));
-}
-}
-);
-});
-});
-});
-}, { inputMode: ‘buffer’ });
+  return new Promise((resolve, reject) => {
+    tmp.tmpName((err, tmpFile) => {
+      if (err) throw err;
+      fs.writeFile(tmpFile, buffer, (err) => {
+        if (err) throw err;
+        im.identify(['-format', '{"width": %w, "height": %h}', tmpFile],
+          (err, output) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(JSON.parse(output));
+            }
+          }
+        );
+      });
+    });
+  });
+}, { inputMode: 'buffer' });
 ~~~
