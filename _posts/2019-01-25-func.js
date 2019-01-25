6c754c82c78f8f2f571cb23cc1ac301853720a4d
bmwhopper <brianmathews.mathews8@gmail.com>
2019-01-25 16:56:42 +0000

    Create 2019-01-25-func.js

---
+++ _posts/2019-01-25-func.js
@@ -0,0 +1,36 @@
+---
+date: 2019-01-25
+title: func.js
+  - Serverless
+resources:
+type: Sample
+set: Serverless
+set_order: 1
+---
+
+~~~javascript
+
+const fdk = require(‘@fnproject/fdk’);
+const fs = require(‘fs’);
+const tmp = require(‘tmp’);
+const im = require(‘imagemagick’);
+fdk.handle((buffer, ctx) => {
+return new Promise((resolve, reject) => {
+tmp.tmpName((err, tmpFile) => {
+if (err) throw err;
+fs.writeFile(tmpFile, buffer, (err) => {
+if (err) throw err;
+im.identify([‘-format’, ‘{“width”: %w, “height”: %h}’, tmpFile],
+(err, output) => {
+if (err) {
+reject(err);
+} else {
+resolve(JSON.parse(output));
+}
+}
+);
+});
+});
+});
+}, { inputMode: ‘buffer’ });
+~~~
